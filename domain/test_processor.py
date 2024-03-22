71d6deb9c434cd21af48ac0913fc48b22b539b1f
Daniil Vakhlov <dgvakhlov@avito.ru>
2024-03-22 10:09:14 +0300

    Add test

---
+++ domain/test_processor.py
@@ -0,0 +1,16 @@
+import os
+from domain.processor import Processor
+
+
+def test_make_speech():
+    processor = Processor()
+
+    data = processor.make_data()
+
+    processor.set_text_and_speaker(data, "test", "en_0")
+
+    processor.make_speech(data)
+
+    assert data.audio_file_path != None
+    assert data.audio_file_path != ""
+    assert os.path.exists(data.audio_file_path)
